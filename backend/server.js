e2a0ed7432db77b85df64089262fe374e83076c9
zikrina <rina1132003@gmail.com>
2025-11-25 14:57:41 +0700

    siap deploy

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const apiRoutes = require("./routes/api");
+require("dotenv").config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api", apiRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server Belimbing Bank berjalan di http://localhost:${PORT}`);
+});
